c5d913bd5dc262c0459f40f22773fde73d0286b7
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-05-05 10:07:56 +0200

    Replace tabs with spaces.

--- cat-tag-post.php
+++ cat-tag-post.php
@@ -8,8 +8,8 @@
 add_shortcode( 'cat_tag_post', 'cat_tag_post' );
 
 function cat_tag_post() {
-	$args = array( 'show_count' => true );
-	ob_start();
+    $args = array( 'show_count' => true );
+    ob_start();
     wp_list_categories( $args );
-	return ob_get_clean();
+    return ob_get_clean();
 }
