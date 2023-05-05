c14850fac085d499a5f8f2c8fa5ed2ab483a9d68
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-05-05 10:02:05 +0200

    Remove demo function.

--- cat-tag-post.php
+++ cat-tag-post.php
@@ -5,9 +5,6 @@
  * Author URI: https://christaylordeveloper.co.uk
  */
 
-function foobar_func( $atts ){
-	return "foo and bar";
-}
 add_shortcode( 'cat_tag_post', 'cat_tag_post' );
 
 function cat_tag_post() {
