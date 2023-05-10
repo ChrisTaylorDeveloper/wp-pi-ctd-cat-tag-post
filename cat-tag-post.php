52485e6c342af1101833d3757df73ac1d50bc000
Chris Taylor <chris@christaylordeveloper.co.uk>
2023-05-10 09:46:41 +0200

    Add a list of posts to each category.

--- cat-tag-post.php
+++ cat-tag-post.php
@@ -1,14 +1,22 @@
 <?php
+declare(strict_types=1);
 /**
  * Plugin Name: Cat Tag Post
+ * Description: Display a hierarchical list of Categories, Sub Categories and Posts.
  * Author: Chris Taylor
  * Author URI: https://christaylordeveloper.co.uk
  */
 
+require 'class-walker-category-and-post.php';
+
 add_shortcode( 'cat_tag_post', 'cat_tag_post' );
 
 function cat_tag_post() {
-    $args = array( 'show_count' => true );
+    $args = array(
+        'show_count' => true,
+        'walker' => new Walker_Category_And_Post()
+    );
+
     ob_start();
     wp_list_categories( $args );
     return ob_get_clean();
---
+++ class-walker-category-and-post.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+class Walker_Category_And_Post extends Walker_Category {
+
+	public function start_el( &$output, $category, $depth = 0, $args = array(), $current_object_id = 0 ) {
+
+		$link = '<strong><a href="' . get_term_link( $category ) . '">' . $category->name . '</a></strong>';
+        $output .= "\t<li>$link\n";
+
+        $output .= $this->category_post_list( $category->cat_ID );
+    }
+
+    private function category_post_list( int $category ): string
+    {
+        $posts_in_category = get_posts( array( 'category' => $category ) );
+
+        $html = '<ul>';
+
+        foreach( $posts_in_category as $post ) {
+            $html .= '<li>';
+            $html .= '<a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a>' ;
+            $html .= '</li>';
+        }
+
+        $html .= '</ul>';
+
+        return $html;
+    }
+}
